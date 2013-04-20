295a61c40b1b91571e91e3e58e64c6adae90c98c
lmd59 <lmd59@cornell.edu>
2013-04-20 19:32:14 -0400

    Secure Controller cleanup

--- bones-web/src/main/java/org/bones/controller/SecureController.java
+++ bones-web/src/main/java/org/bones/controller/SecureController.java
@@ -1,20 +1,7 @@
 package org.bones.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
-import org.bones.model.User;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
-import org.hibernate.criterion.Restrictions;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,34 +16,10 @@ public class SecureController {
 	public String getProfile(ModelMap model) {
  
 		System.out.println("Getting current user profile");
-        
-        try{
-        	User user = null;
-            Session hibSession = getHibernateSession();
-            Transaction tx = hibSession.beginTransaction();
-            user = (User) hibSession.createCriteria(User.class)
-                    .add( Restrictions.eq("username", "alex") ).uniqueResult();
-        	tx.commit();
-            System.out.println("currUser name: " + user.getUsername());
-            model.addAttribute("currUser",user);
-        }catch(Exception e){
-            System.out.println("Exception: " + e);
-        }
+        //TODO: get current user profile with DAO
 		return "secure/user-profile";
  
 	}
 	
-	private Session getHibernateSession(){
-        Session hibSession = null;
-        try{
-            //Hibernate Objects
-            Configuration cfg = new Configuration();
-            SessionFactory sf = cfg.configure().buildSessionFactory();
-            hibSession = sf.openSession();
-        }catch(Exception e){
-            System.out.println("Exception: " + e);
-        }
-        return hibSession;
-    }
 	
 }
